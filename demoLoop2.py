9aa80d8c4b97b4394109adda028cfdb66035a481
chaehunim996 <chaehunim996@gmail.com>
2025-04-14 16:45:43 +0900

    월요일 수업 완료

--- demoLoop2.py
+++ demoLoop2.py
@@ -2,3 +2,38 @@
 
 for item in [1,2,3]:
     print(item)
+
+#수열 함수
+print( list(range(1,11)) )
+print( list(range(2000,2026)))
+print( list(range(1,32)))
+
+#리스트 내장
+lst = list(range(1,11))
+print(lst)
+print( [i**2 for i in lst if i > 5])
+tp = ("apple","kiwi")
+print( [len(i) for i in tp])
+d = {100:"apple", 200:"kiwi"}
+print( [v.upper() for v in d.values()])
+
+print("--- 필터링 함수---")
+lst = [10, 25, 30]
+itemL = filter(None, lst)
+for item in itemL:
+    print(item)
+
+print("--- 필터링 함수 사용---")
+def getBiggerThan20(i):
+    return i > 20
+    
+lst = [10, 25, 30]
+itemL = filter(getBiggerThan20, lst)
+for item in itemL:
+    print(item)
+
+print("--- 람다 함수 사용---")
+lst = [10, 25, 30]
+itemL = filter(lambda x:x>20, lst)
+for item in itemL:
+    print(item)
